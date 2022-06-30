243b935804be736b1e70c18af9d061d3a50591d1
Hariomsingh333 <www.hariom03.com@gmail.com>
2022-07-01 00:24:48 +0530

    add temp and all those temp thing

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -2,9 +2,31 @@ import React, { useContext } from "react";
 import createContextFunction from "../context/createContextFunction";
 
 const WeatherData = () => {
-  const { DummyData } = useContext(createContextFunction);
-  // console.log(DummyData);
-  return <div>hehe</div>;
+  const { Data } = useContext(createContextFunction);
+  const url = `https://openweathermap.org/img/wn/${Data.weather[0].icon}@2x.png`;
+  console.log(Data);
+  return (
+    <div>
+      <section>
+        <div className="flex flex-row justify-between items-center ">
+          {/* Name  */}
+          <div className="mx-2 text-lg font-mono font-bold capitalize">
+            {Data.name}
+          </div>
+
+          {/* icon  */}
+          <div>
+            <img src={url} alt="Weather-icon" />
+          </div>
+        </div>
+        {/* temp  */}
+
+        <p>Temperature: {Data.main.temp}℃</p>
+        <p>Feel Like: {Data.main.feels_like}℃</p>
+        <p>{Data.weather[0].main}</p>
+      </section>
+    </div>
+  );
 };
 
 export default WeatherData;
--- src/context/CreateContext.jsx
+++ src/context/CreateContext.jsx
@@ -3,34 +3,31 @@ import React, { useEffect, useState } from "react";
 import createContextFunction from "./createContextFunction";
 
 const CreateContext = ({ children }) => {
+  const [isCityName, setIsCityName] = useState(false);
   const [cityName, setCityName] = useState("");
   const getCityName = (city) => {
     setCityName(city);
+    setIsCityName(true);
   };
-  const DummyData = [
-    {
-      temp: 44,
-      city: cityName,
-    },
-  ];
   const [weatherData, setWeatherData] = useState([]);
   const apitkey = "30c06c817dfaf455478e39636b9e5619";
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apitkey}`;
   useEffect(() => {
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apitkey}`;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${
+      isCityName ? cityName : "Kolkata"
+    }&units=metric&appid=${apitkey}`;
     // console.log(api);
     const getData = async () => {
       const res = await axios.get(api);
       const data = await res.data;
       setWeatherData(data);
     };
-
     getData();
   }, [cityName]);
   return (
     <createContextFunction.Provider
       value={{
-        Data: DummyData,
+        // Data: DummyData,
+        Data: weatherData,
         getCityName: getCityName,
       }}
     >
